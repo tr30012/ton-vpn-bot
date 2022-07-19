11badc0269d3fd58de69bc2e94c3e8e390b6adb4
hotaged <72806811+tr30012@users.noreply.github.com>
2022-07-19 03:39:05 +0300

    Added swagger autodoc

--- vpnbot/api/app.py
+++ vpnbot/api/app.py
@@ -1,6 +1,7 @@
 from aiohttp import web
-from api.handler import handler
+from api.handler import health
 from settings import create_logger
+from aiohttp_apispec import setup_aiohttp_apispec
 
 logger = create_logger("app.creator")
 
@@ -9,7 +10,11 @@ def create_app(**kwargs) -> web.Application:
     logger.info("Creating app.")
 
     app = web.Application()
-    app.add_routes([web.post("/api", handler)])
+    app.add_routes([web.post("/health", health)])
+
+    setup_aiohttp_apispec(
+        app=app, title="VPN Bot API", swagger_path="/"
+    )
 
     for kwarg in kwargs:
         app[kwarg] = kwargs[kwarg]
--- vpnbot/api/handler.py
+++ vpnbot/api/handler.py
@@ -1,5 +1,11 @@
 from aiohttp import web
+from aiohttp_apispec import docs
 
 
-async def handler(request: web.Request) -> web.Response:
+@docs(
+    tags=["health-check"],
+    summary="Simple health check",
+    description="Check of bot api",
+)
+async def health(request: web.Request) -> web.Response:
     return web.json_response({"status": "OK"}, status=200)
